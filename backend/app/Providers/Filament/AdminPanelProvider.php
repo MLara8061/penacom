6f20b7f8f40178f45c8f9dd0d53d760516dfc8ba
MLara8061 <mario.lara.martin@gmail.com>
2025-11-29 16:56:09 -0500

    Remove logo reference from Filament admin panel

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -31,8 +31,6 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Blue,
             ])
             ->brandName('Peñacom')
-            ->brandLogo(asset('images/logo.png'))
-            ->brandLogoHeight('2.5rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
